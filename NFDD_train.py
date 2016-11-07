441c1dc0917334314a3697c283121d97691aea81
Pandaman-Ryan <anz023@eng.ucsd.com>
2016-11-07 15:27:07 -0800

    updata_NFDD_train.py

--- NFDD_train.py
+++ NFDD_train.py
@@ -85,6 +85,7 @@ model.add(Activation('sigmoid'))
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
+
 model.fit(X_train, y_train,
           batch_size=batch_size,
           nb_epoch=nb_epoch,
